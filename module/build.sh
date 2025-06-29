bf6d503cf4c48362dcdf8fdc90350fa558613a82
felix-sky <felixfengzitao@gmail.com>
2025-06-29 23:31:50 +0200

    fix building errors

--- module/build.sh
+++ module/build.sh
@@ -1,3 +1,7 @@
 #/bin/bash
 
-pyinstaller --onefile sav_cli.py -n sav_cli_$(uname -s | tr 'A-Z' 'a-z')_$(uname -m | tr 'A-Z' 'a-z')
+pyinstaller --onefile sav_cli.py \
+  -n sav_cli_$(uname -s | tr 'A-Z' 'a-z')_$(uname -m | tr 'A-Z' 'a-z') \
+  --add-data "./palworld_save_tools/libs/oodle/libs/Linux/liboo2corelinux64.so.9:palworld_save_tools/libs/oodle/libs/Linux" \
+  --add-data "./palworld_save_tools/libs/oodle/libs/Linux/liboo2extlinux64.so.9:palworld_save_tools/libs/oodle/libs/Linux" \
+  --add-binary "/usr/lib/x86_64-linux-gnu/libpython3.10.so.1.0:."
